6998720b19ade7302c7cb22372691db1902adcc4
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2025-04-16 14:44:12 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.01 ms, Memory: 4.36 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int GetLength(const char* string)
+{
+	int i = 0;
+	while (string[i] != '\0')
+	{
+		i++;
+	}
+	return i;
+}
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* num_str) {
+    int digit = GetLength(num_str);
+    int answer = 0;
+
+    for (int i = 0; i < digit; i++)
+    {
+        answer += (num_str[i] - '0');
+    }
+    return answer;
+}
