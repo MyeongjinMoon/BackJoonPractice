a685ab7cde564dfe8eac1152e5d14ab105443551
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2025-04-22 15:06:14 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 120 ms, Memory: 10340 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+
+int n, k;
+cin >> n >> k;
+
+int** map = (int**)calloc(sizeof(int*), n + 1);
+for (int i = 0; i <= n; i++)
+	map[i] = (int*)calloc(sizeof(int), n + 1);
+
+int** sum_map = (int**)calloc(sizeof(int*), n + 1);
+for (int i = 0; i <= n; i++)
+	sum_map[i] = (int*)calloc(sizeof(int), n + 1);
+
+for (int i = 1; i <= n; i++)
+	for (int j = 1; j <= n; j++)
+	{
+		int num;
+		cin >> num;
+
+		map[i][j] = num;
+
+		sum_map[i][j] = map[i][j] + sum_map[i][j - 1] + sum_map[i - 1][j] - sum_map[i - 1][j - 1];
+	}
+
+for (int i = 0; i < k; i++)
+{
+	int s_x, s_y, e_x, e_y;
+	cin >> s_y >> s_x >> e_y >> e_x;
+
+	int answer = 0;
+	answer = sum_map[e_y][e_x] - sum_map[e_y][s_x - 1] - sum_map[s_y - 1][e_x] + sum_map[s_y-1][s_x-1];
+	cout << answer << '\n';
+}
+}
